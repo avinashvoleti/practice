81538b451aa73ccf32567f055a039a51f032d23e
avinashvoleti <58489506+avinashvoleti@users.noreply.github.com>
2020-03-02 15:41:53 +0530

    Update TestUtil.java

--- src/main/java/util/qa/pages/TestUtil.java
+++ src/main/java/util/qa/pages/TestUtil.java
@@ -5,6 +5,7 @@ import baseclass.qa.pages.Testbase;
 public class TestUtil extends Testbase {
  public static long PAGE_LOAD_TIMEOUT = 40000;
  public static long IMPLICIT_WAIT = 3000;
+	public static long test = 2000;
 
 
 public void switchtoframe() {
